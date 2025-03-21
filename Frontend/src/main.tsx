23bae8bc4bd34a25d6937e6a8b7d18210ca6f9ae
EudesNilton <eudes258@gmail.com>
2025-03-21 10:14:44 -0300

    docs: atualização

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -3,8 +3,29 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import {createBrowserRouter, RouterProvider, Route} from "react-router-dom"
+import Login from './pages/Login.tsx'
+import ErrorPage from './pages/ErrorPage.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "vagas",
+    element: <ErrorPage />,
+  },
+  {
+    path: "login",
+    element: <Login />,
+  }
+  
+  ])
+
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
